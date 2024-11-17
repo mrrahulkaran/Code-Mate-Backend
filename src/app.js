1762c44a94dad0a9be4672b0f1533e14ad8d92e6
mrrahulkaran <karanr9991@gmail.com>
2024-11-17 16:06:41 +0530

    routing done and learn advance routing techniques

--- src/app.js
+++ src/app.js
@@ -1,24 +1,37 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-app.get("/home", function (req, res) {
-  res.send("welcome home");
-});
-
-app.get("/data", function (req, res) {
-  res.send("no data right now");
-});
-
-/* app.get("/app", function (req, res) {
+app.get("/app", function (req, res) {
   res.send("in progress");
-}); */
+});
+
+app.post("/user", function (req, res) {
+  res.send("user updated in data base");
+});
+
+app.get("/user", function (req, res) {
+  res.send({ name: "rahul", phone: "7388378005" });
+});
+
+//advance rounting concepts
+//we can use regularexpression in routing string
+
+app.get("/ab?c", function (req, res) {
+  res.send("abc");
+});
+
+app.get("/ab+c", function (req, res) {
+  res.send("abc");
+});
+
+app.get("/ab*cd", function (req, res) {
+  res.send("abcd");
+});
+
+app.get("/a(bc)?d", function (req, res) {
+  res.send("abcd");
+});
 
 app.listen(3000, () => {
   console.log("server is running on 3000 port");
 });
-
-// console.log("starting the project");
