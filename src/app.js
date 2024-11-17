069e8251fee7f1cd39023b92fb30243868afca89
mrrahulkaran <karanr9991@gmail.com>
2024-11-17 20:20:03 +0530

    Perform different type of rout dynamic regexx and reading query param and reading the dynamic routs

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ app.get("/app", function (req, res) {
   res.send("in progress");
 });
 
-app.post("/user", function (req, res) {
+/* app.post("/user", function (req, res) {
   res.send("user updated in data base");
-});
+}); */
 
-app.get("/user", function (req, res) {
+/* app.get("/user", function (req, res) {
   res.send({ name: "rahul", phone: "7388378005" });
-});
+}); */
 
 //advance rounting concepts
 //we can use regularexpression in routing string
@@ -30,6 +30,29 @@ app.get("/ab*cd", function (req, res) {
 
 app.get("/a(bc)?d", function (req, res) {
   res.send("abcd");
+});
+
+//regexx routing
+
+/* app.get(/a/, function (req, res) {
+  res.send("abcd");
+}); */
+app.get(/.*fly$/, function (req, res) {
+  res.send("any thing before fly");
+});
+
+// how to read (get user id) value from query parameters
+
+/* app.get("/user", function (req, res) {
+  console.log(req.query);
+  res.send({ firstname: "rahul", lastname: "karan" });
+}); */
+
+// how to read user id from dynamic rout ( : coulumn means dynimic routs)
+
+app.get("/user/:userid", function (req, res) {
+  console.log(req.params);
+  res.send({ firstname: "akshay", lastname: "karan" });
 });
 
 app.listen(3000, () => {
