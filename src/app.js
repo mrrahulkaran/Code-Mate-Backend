886bff5e9bc1d91823287a5e102bcd1f80cc7bf2
mrrahulkaran <karanr9991@gmail.com>
2024-11-20 17:14:34 +0530

    Validated the schema and senetization of data base done exprored mongooose Documentation

--- src/app.js
+++ src/app.js
@@ -7,10 +7,21 @@ app.use(express.json());
 
 //API -- sign up
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-  console.log(req.body);
-  await user.save();
-  res.send("User added succesessfully");
+  try {
+    const user = new User(req.body);
+
+    const emailIDExists = await User.findOne({ emailId: req.body.emailId });
+    if (emailIDExists) return res.status(400).send("Email already taken");
+
+    if (user.Password.length < 7)
+      return res.status(400).send("Password must contain 7 Charactor");
+
+    await user.save();
+    console.log(user);
+    res.send("wooohoo....Profile Created ");
+  } catch (error) {
+    res.status(400).send("SOME ERROR ACCOURS: " + error);
+  }
 });
 
 // API -- feed api to see all the users in DB
@@ -42,9 +53,14 @@ app.get("/user", async (req, res) => {
 
 app.delete("/user", async (req, res) => {
   const emailId = req.body.emailId;
+
+  const emailIDExists = await User.findOne({ emailId: req.body.emailId });
+  if (!emailIDExists) return res.status(400).send("Email Not Exist");
+  console.log(emailId.value);
   try {
-    const user = await User.findOneAndDelete(emailId);
+    await User.findOneAndDelete({ emailId: emailId });
     res.send("user deleted succsessfully");
+    console.log(emailId.value);
   } catch (error) {
     res.status(404).send("User not found");
   }
@@ -52,16 +68,38 @@ app.delete("/user", async (req, res) => {
 
 // API- update a user
 app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
-  const data = req.body;
-
   try {
+    const userId = req.body.userId;
+    const data = req.body;
+    console.log(userId);
+    console.log(data);
+    const ALLOWED_UPDATES = [
+      "lastName",
+      "Password",
+      "photoUrl",
+      "about",
+      "skills",
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (isUpdateAllowed) return res.status(400).send("Can Not Update fields");
+    if (data?.skills.lenght > 10)
+      return res.status(400).send("Skills Not more than 10");
+
     await User.findByIdAndUpdate({ _id: userId }, data);
+
     res.send("user updated successfully");
-  } catch (err) {
-    res.status(400).send("something went wrong");
+  } catch (Error) {
+    res.status(400).send("something went wrong" + Error);
   }
 });
+
+/* app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
+}); */
 
 connenctDB()
   .then(() => {
--- src/model/user.js
+++ src/model/user.js
@@ -3,15 +3,28 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
   },
   lastName: {
     type: String,
+    trim: true,
+
+    maxLength: 50,
   },
   emailId: {
     type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
   },
   Password: {
     type: String,
+    required: true,
+    minLength: 7,
   },
 
   age: {
@@ -19,6 +32,24 @@ const userSchema = new mongoose.Schema({
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male", "female", "other"].includes(value)) {
+        throw new Error("gender data is not valid");
+      }
+    },
+  },
+  photoUrl: {
+    type: String,
+    default:
+      "https://cdn2.iconfinder.com/data/icons/business-hr-and-recruitment/100/account_blank_face_dummy_human_mannequin_profile_user_-512.png",
+  },
+
+  about: {
+    type: String,
+    default: "hi there",
+  },
+  skills: {
+    type: [String],
   },
 });
 
