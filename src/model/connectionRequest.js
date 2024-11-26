063c774cd290723ab119da50ca3a57e730d9e6b9
mrrahulkaran <karanr9991@gmail.com>
2024-11-26 18:49:02 +0530

    API Code Correction And bug fixx

--- src/model/connectionRequest.js
+++ src/model/connectionRequest.js
@@ -8,6 +8,7 @@ const connectionRequestSchema = new mongoose.Schema(
     },
     reciverId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     status: {
--- src/routes/userrouter.js
+++ src/routes/userrouter.js
@@ -6,14 +6,51 @@ const { UserAuth } = require("../Middelwares/auth.js");
 userRouter.get("/user/request", UserAuth, async (req, res) => {
   const loginuser = req.user;
   try {
+    const USER_SAFE_DATA = [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "age",
+      "skills",
+    ];
     const connections = await ConnectionRequest.find({
       reciverId: loginuser._id,
       status: "intrested",
-    }).populate("senderId", ["firstName", "lastName"]);
-
-    console.log(connections);
-
+    }).populate("senderId", USER_SAFE_DATA);
     res.json({ message: "Data fetched Succsessfully ", data: connections });
+  } catch (error) {
+    res.status(400).send("Opps: " + error.message);
+  }
+});
+
+userRouter.get("/user/all/connections", UserAuth, async (req, res) => {
+  try {
+    const loginuser = req.user;
+    const USER_SAFE_DATA = [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "age",
+      "skills",
+    ];
+
+    const connectionsReq = await ConnectionRequest.find({
+      $or: [
+        { receverId: loginuser._id, status: "accsepted" },
+        { senderId: loginuser._id, status: "accsepted" },
+      ],
+    })
+      .populate("senderId", USER_SAFE_DATA)
+      .populate("reciverId", USER_SAFE_DATA);
+
+    const data = connectionsReq.map((row) => {
+      if (row.senderId._id.toString() === loginuser._id.toString()) {
+        return row.receverId;
+      }
+      return row.senderId;
+    });
+
+    res.json({ data: data });
   } catch (error) {
     res.status(400).send("Opps: " + error.message);
   }
